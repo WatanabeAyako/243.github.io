f86c5d1b92b74d4d0c02c90324a3b2bac2e31130
AW <103615612+WatanabeAyako@users.noreply.github.com>
2023-04-23 20:29:59 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+const p1Button = document.querySelector('#p1Button');
+const p2Button = document.querySelector('#p2Button');
+const resetButton = document.querySelector('#reset');
+const winningScoreSelect = document.querySelector('#winningScore');
+const p1Display = document.querySelector('#p1Display');
+const p2Display = document.querySelector('#p2Display')
+
+let p1Score = 0
+let p2Score = 0
+let winningScore = 25;
+let isGameOver = false;
+
+p1Button.addEventListener('click', function(){
+    if (!isGameOver) {
+        p1Score +=1;
+        p1Display.textContent = p1Score;
+        if (p1Score === winningScore){
+            isGameOver = true;
+            p1Display.classList.add('has-text-success');
+            p2Display.classList.add('has-text-danger')
+            p1Button.disabled = true;
+            p2Button.disabled = true;
+        }    
+    }
+});
+
+p2Button.addEventListener('click', function(){
+    if (!isGameOver) {
+        p2Score +=1;
+        p2Display.textContent = p2Score;
+        if (p2Score === winningScore){
+            isGameOver = true;
+            p2Display.classList.add('has-text-success');
+            p1Display.classList.add('has-text-danger');
+            p1Button.disabled = true;
+            p2Button.disabled = true;
+        }    
+    }
+});
+
+winningScoreSelect.addEventListener('change', function(){
+    winningScore = parseInt(this.Value); 
+    reset();
+});
+
+resetButton.addEventListener('click', reset);
+
+function reset(){
+    isGameOver = false;
+    p1Score = 0;
+    p2Score = 0;
+    p1Display.textContent = 0;
+    p2Display.textContent = 0;
+    p1Display.classList.remove('winner', 'loser');
+    p2Display.classList.remove('winner', 'loser');
+    p1Button.disabled = false;
+    p2Button.disabled = false;
+
+}
